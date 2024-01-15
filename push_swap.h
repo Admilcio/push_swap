79dbdac8092966ff3973bcf78723eafa5625ef7e
Admilcio da Mata <113069526+Admilcio@users.noreply.github.com>
2024-01-15 17:54:11 +0000

    Update 42 header

--- push_swap.h
+++ push_swap.h
@@ -1,10 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ada-mata <ada-mata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 14:33:07 by ada-mata          #+#    #+#             */
+/*   Updated: 2024/01/15 17:35:38 by ada-mata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 # include "./libft/libft.h"
 
-void	printList(t_list *head);
 int		ft_lstsize(t_list *head);
 
 void	ft_error(char *msg);
@@ -19,22 +29,21 @@ void	ft_sort(t_list **stack_a, t_list **stack_b);
 void	index_stack(t_list **stack);
 
 // Instruction functions
-void		swap(t_list **stack);
-void		rotate(t_list **stack);
-void		reverseRotate(t_list **stack);
+void	swap(t_list **stack);
+void	rotate(t_list **stack);
+void	reverse_rotate(t_list **stack);
 
-void		sa(t_list **stack_a);
-void		sb(t_list **stack_b);
-void		ss(t_list **stack_a, t_list **stack_b);
-void		pa(t_list **stack_a, t_list **stack_b);
-void		pb(t_list **stack_b, t_list **stack_a);
-void		ra(t_list **stack_a);
-void		rb(t_list **stack_b);
-void		rr(t_list **stack_a, t_list **stack_b);
-void		rra(t_list **stack_a);
-void		rrb(t_list **stack_b);
-void		rrr(t_list **stack_a, t_list **stack_b);
-int			get_min(t_list *stack);
+void	sa(t_list **stack_a);
+void	sb(t_list **stack_b);
+void	ss(t_list **stack_a, t_list **stack_b);
+void	pa(t_list **stack_a, t_list **stack_b);
+void	pb(t_list **stack_b, t_list **stack_a);
+void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rr(t_list **stack_a, t_list **stack_b);
+void	rra(t_list **stack_a);
+void	rrb(t_list **stack_b);
+void	rrr(t_list **stack_a, t_list **stack_b);
+int		get_min(t_list *stack);
 
 #endif
-
