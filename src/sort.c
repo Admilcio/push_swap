8d551e1f8693baf4ab352c930c1a1a3ff3a4423a
Admilcio da Mata <113069526+Admilcio@users.noreply.github.com>
2024-01-19 14:27:58 +0000

    Update sort.c according to norm

--- src/sort.c
+++ src/sort.c
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ada-mata <ada-mata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 14:23:05 by ada-mata          #+#    #+#             */
+/*   Updated: 2024/01/18 17:34:38 by ada-mata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../push_swap.h"
 
 int	get_min(t_list *stack)
 {
-	t_list *tmp;
-	int min;
+	t_list	*tmp;
+	int		min;
 
 	tmp = stack;
 	min = tmp->value;
@@ -28,7 +39,7 @@ static void	sort_3(t_list **stack_a)
 	}
 }
 
-int	ft_rotate_direction(t_list *first_node)
+int	ft_rotate_direction(t_list *a)
 {
 	int	smallest;
 	int	index;
@@ -38,29 +49,30 @@ int	ft_rotate_direction(t_list *first_node)
 	index = 0;
 	smallest_index = 0;
 	smallest = 2147483647;
-	list_size = ft_lstsize(first_node) / 2;
-	while (first_node)
+	list_size = ft_lstsize(a) / 2;
+	while (a)
 	{
-		if (first_node->value < smallest)
+		if (a->value < smallest)
 		{
-			smallest = first_node->value;
+			smallest = a->value;
 			smallest_index = index;
 		}
-		first_node = first_node->next;
+		a = a->next;
 		index++;
 	}
 	if (smallest_index < list_size)
 		return (1);
 	else
 		return (-1);
-} 
+}
 
-void sort_4_5(t_list **stack_a, t_list **stack_b)
+void	sort_4_5(t_list **stack_a, t_list **stack_b)
 {
+	int	min_value;
+
 	while (ft_lstsize(*stack_a) > 3)
 	{
-		int min_value = get_min(*stack_a);
-
+		min_value = get_min(*stack_a);
 		while ((*stack_a)->value != min_value)
 		{
 			if (ft_rotate_direction(*stack_a) == 1)
@@ -68,14 +80,11 @@ void sort_4_5(t_list **stack_a, t_list **stack_b)
 			else
 				rra(stack_a);
 		}
-
 		pb(stack_a, stack_b);
 	}
-
-    sort_3(stack_a);
-
-    while (ft_lstsize(*stack_b) > 0)
-        pa(stack_a, stack_b);
+	sort_3(stack_a);
+	while (ft_lstsize(*stack_b) > 0)
+		pa(stack_a, stack_b);
 }
 
 void	ft_sort(t_list **stack_a, t_list **stack_b)
@@ -93,4 +102,3 @@ void	ft_sort(t_list **stack_a, t_list **stack_b)
 	else if (size >= 4 && size <= 5)
 		sort_4_5(stack_a, stack_b);
 }
-
