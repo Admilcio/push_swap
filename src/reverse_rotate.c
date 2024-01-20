d29c98bc62e8146b3e39fcf8c43e8214a3894e7d
Admilcio da Mata <113069526+Admilcio@users.noreply.github.com>
2024-01-20 17:12:39 +0000

    Update reverse_rotate.c according to norm

--- src/reverse_rotate.c
+++ src/reverse_rotate.c
@@ -1,11 +1,24 @@
-#include "push_swap.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ada-mata <ada-mata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 14:22:55 by ada-mata          #+#    #+#             */
+/*   Updated: 2024/01/15 17:39:17 by ada-mata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-// Shifts down all elements of a stack by 1. The last element becomes the first one | rra and rrb
+#include "../push_swap.h"
 
-void	reverseRotate(t_list **stack)
+// Shifts down all elements of a stack by 1. 
+// The last element becomes the first one | rra and rrb
+
+void	reverse_rotate(t_list **stack)
 {
-  t_list	*tmp;
-	t_list	*tmplast;
+	t_list		*tmp;
+	t_list		*tmplast;
 
 	tmplast = ft_lstlast(*stack);
 	tmp = (*stack);
@@ -18,19 +31,19 @@ void	reverseRotate(t_list **stack)
 
 void	rra(t_list **stack_a)
 {
-	reverseRotate(stack_a);
+	reverse_rotate(stack_a);
 	ft_putendl_fd("rra", 1);
 }
 
 void	rrb(t_list **stack_b)
 {
-	reverseRotate(stack_b);
+	reverse_rotate(stack_b);
 	ft_putendl_fd("rrb", 1);
 }
 
 void	rrr(t_list **stack_a, t_list **stack_b)
 {
-	reverseRotate(stack_a);
-	reverseRotate(stack_b);
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
 	ft_putendl_fd("rrr", 1);
 }
